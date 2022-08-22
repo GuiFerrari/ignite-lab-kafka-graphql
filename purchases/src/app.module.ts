3ab63d85ce8716f9afd6137b3546c96332e3ee10
Guilherme Ferrari <guiferrari10@hotmail.com>
2022-08-22 17:40:58 -0300

    setup: created projects with nextjs, nestjs, kafka and prisma

---
+++ purchases/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { DatabaseModule } from './database/database.module';
+import { HttpModule } from './http/http.module';
+import { PrismaService } from './prisma/prisma.service';
+
+@Module({
+  imports: [DatabaseModule, HttpModule],
+  controllers: [],
+  providers: [PrismaService],
+})
+export class AppModule {}
